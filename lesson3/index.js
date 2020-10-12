de8fc56090be4f330b7f83e5edeb118994c81821
pDallastra <prpdallastra@gmail.com>
2020-10-12 11:44:35 -0300

    Lesson 03

---
+++ lesson3/index.js
@@ -0,0 +1,17 @@
+function setTimes() {
+  var date = new Date();
+  var copyYear = date.getFullYear();
+  var lastModified = document.lastModified;
+  
+  document.getElementById('copyYear').innerHTML = copyYear
+  document.getElementById('lastModified').innerHTML = lastModified
+}
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
